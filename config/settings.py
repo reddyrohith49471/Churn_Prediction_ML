40f72c6b8fee5a4a21df3582ce038e13b46701f1
reddyrohith49471 <145396409+reddyrohith49471@users.noreply.github.com>
2026-01-12 22:46:33 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = BASE_DIR / "data"
 
 SCHEMA_PATH = BASE_DIR / "config" / "schemas.yaml"
-LIVE_DATA = BASE_DIR / "data" / "incoming" / "live_data.csv"
+LIVE_DATA = BASE_DIR / "incoming" / "live_data.csv"
 MODEL_PATH = BASE_DIR / "artifacts" / "rfc_tuned_model.joblib"
 
 MODEL_VERSION = "RFC_TUNED_V1"
 
-TRAINING_DATA = BASE_DIR / "data" / "reference" / "WA_Fn-UseC_-Telco-Customer-Churn 2.csv"
+TRAINING_DATA = BASE_DIR / "reference" / "WA_Fn-UseC_-Telco-Customer-Churn 2.csv"

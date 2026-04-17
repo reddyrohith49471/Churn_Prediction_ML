7a66df798d66e4348342cb33129edb8efa551032
reddyrohith49471 <145396409+reddyrohith49471@users.noreply.github.com>
2026-04-18 00:11:57 +0530

    Update fastapi_app.py

--- fastapi_app.py
+++ fastapi_app.py
@@ -6,6 +6,7 @@ from fastapi.templating import Jinja2Templates
 from helper import ChurnService
 
 
+
 app = FastAPI(
     title="Customer Churn Prediction",
     description="Web-based customer churn prediction system",
